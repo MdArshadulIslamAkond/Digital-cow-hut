3ad932aa90bcd2c82e23d7999899491a8e306d4c
Arshadul-Islam <arshadul36@gmail.com>
2025-01-28 17:19:21 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from 'eslint-config-prettier';
+// import pluginReact from "eslint-plugin-react";
+import jsdoc from "eslint-plugin-jsdoc";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.node }},
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  {
+    ignores: [".node_modules/*"]
+  },
+  {
+    plugins: {
+      jsdoc,
+    },
+   
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  // pluginReact.configs.flat.recommended,
+];
